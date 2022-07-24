39dbfe76fce589dfc45ad36bbb48f26d8df49753
Venkat Kamani <kamanivenkat@gmail.com>
2022-07-24 13:28:23 +0530

    add singleton class in WebDriverUtility.java

--- src/test/java/module1Tests/Module1Tests.java
+++ src/test/java/module1Tests/Module1Tests.java
@@ -3,8 +3,11 @@ package module1Tests;
 
 import libraries.TextFileUtility;
 import libraries.WebDriverUtility;
+import org.apache.groovy.json.internal.Exceptions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import pageObjects.Module1Page;
@@ -12,6 +15,7 @@ import pageObjects.Module1Page;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
 
 public class Module1Tests {
@@ -21,31 +25,38 @@ public class Module1Tests {
     @Test
     public void test1() throws InterruptedException, IOException {
         webDriver.get("https://www.youtube.com/channel/UCc79Etb6d6ISwCN4SeQgEJA/videos");
-        for(int i = 0; i<=10000000; i++){
+        for (int i = 0; i <= 10000000; i++) {
             String dateTime = new SimpleDateFormat("EEE dd-MM-yyyy HH:mm:ss.SSS Z").format(new Date());
-            String [] content = module1Page.getSubscribersCount().getText().split("subscribers");
-            System.out.println(dateTime + ", " + content [0].trim() + ", " + "subscribers");
-            Thread.sleep(300000);
             webDriver.navigate().refresh();
-            TextFileUtility.appendContentToTextFile(dateTime + ", " + content [0].trim() + ", " + "subscribers");
+            new WebDriverWait(webDriver, Duration.ofSeconds(10)).
+                    until(ExpectedConditions.textToBePresentInElement(module1Page.getSubscribersCount(),
+                            "subscribers"));
+            String[] content = module1Page.getSubscribersCount().getText().split("subscribers");
+            System.out.println(dateTime + ", " + content[0].trim() + ", " + "subscribers");
+            Thread.sleep(900000);//Every 15 mins
+            webDriver.navigate().refresh();
+            TextFileUtility.appendContentToTextFile(dateTime + ", " + content[0].trim() + ", " + "subscribers");
         }
 
-
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         System.out.println("Test2");
     }
+
     @Test
-    public void test3(){
+    public void test3() {
         System.out.println("Test3");
     }
+
     @Test
-    public void test4(){
+    public void test4() {
         System.out.println("Test4");
     }
+
     @AfterClass
-    public void closeBrowser(){
+    public void closeBrowser() {
         WebDriverUtility.getInstance().quit();
     }
 }
