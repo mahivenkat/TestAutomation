963db2984cf9b7bdaaaeb48900a411ba8c17f263
Venkat Kamani <kamanivenkat@gmail.com>
2022-07-26 15:54:35 +0530

    uploading the logs

--- src/main/java/libraries/TextFileUtility.java
+++ src/main/java/libraries/TextFileUtility.java
@@ -1,6 +1,7 @@
 package libraries;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DateFormat;
@@ -10,9 +11,16 @@ import java.util.Date;
 public class TextFileUtility {
 
     public static void appendContentToTextFile(String content) throws IOException {
-        File file = new File("C:\\Users\\Deepak Dontineni\\me\\untitled\\src\\test\\resources\\OutputData\\kundabaddalu2_0_subscribersNumbers.txt");
+        File file = new File(System.getProperty("user.dir")+"\\src\\test\\resources\\OutputData\\kundabaddalu2_0_subscribersNumbers.txt");
         FileWriter fw = new FileWriter(file, true);
         fw.write(content+"\n");
+        fw.close();
+    }
+
+    public static void readContentToTextFile(String content) throws IOException {
+        File file = new File(System.getProperty("user.dir")+"\\src\\test\\resources\\OutputData\\kundabaddalu2_0_subscribersNumbers.txt");
+        FileReader fw = new FileReader(file);
+        fw.read();
         fw.close();
     }
 
