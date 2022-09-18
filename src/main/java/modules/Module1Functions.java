38b885881d21f27930317540b46b2651098bf52a
Venkat Kamani <kamanivenkat@gmail.com>
2022-09-19 00:51:24 +0530

    added tests to module1

--- src/main/java/modules/Module1Functions.java
+++ src/main/java/modules/Module1Functions.java
@@ -2,7 +2,23 @@ package modules;
 import org.testng.annotations.Test;
 public class Module1Functions {
 
-    public void test1(){
-        System.out.println("module1");
+    @Test
+    public void test1Module1(){
+        System.out.println("test1Module1");
+    }
+
+    @Test
+    public void test2Module1(){
+        System.out.println("test2Module1");
+    }
+
+    @Test
+    public void test3Module1(){
+        System.out.println("test3Module1");
+    }
+
+    @Test
+    public void test4Module1(){
+        System.out.println("test4Module1");
     }
 }
